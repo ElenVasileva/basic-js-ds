0578ae92fed92ea668bba17256d55e724011f5a4
Elena <elen.vasileva@gmail.com>
2024-12-20 00:51:53 +0100

    feat: node deleting

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,11 +1,35 @@
 const { NotImplementedError } = require('../extensions/index.js');
-
 const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
+const findNode = (parentNode, valueToSearch) => {
+  if (!parentNode) {
+    return null;
+  } else if (parentNode.data === valueToSearch) {
+    return parentNode;
+  } else if (parentNode.data > valueToSearch) {
+    return findNode(parentNode.left, valueToSearch);
+  } else {
+    return findNode(parentNode.right, valueToSearch);
+  }
+};
+
+const isLeaf = (node) => {
+  return !node.left && !node.right;
+};
+
+const findMax = (node) => {
+  return node.right ? findMax(node.right) : node.data;
+};
+
+const findMin = (node) => {
+  return node.left ? findMin(node.left) : node.data;
+};
+
 class BinarySearchTree {
   rootNode = null;
 
@@ -55,38 +79,62 @@ class BinarySearchTree {
   }
 
   find(data) {
-    const findNode = (parentNode, valueToSearch) => {
-      if (!parentNode) {
-        return null;
-      } else if (parentNode.data === valueToSearch) {
-        return parentNode;
-      } else if (parentNode.data > valueToSearch) {
-        return findNode(parentNode.left, valueToSearch);
-      } else {
-        return findNode(parentNode.right, valueToSearch);
-      }
-    };
-
     return findNode(this.rootNode, data);
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {    
+
+    const removeLeaf = (parentNode, data) => {
+      if (parentNode.left && parentNode.left.data === data) {
+        parentNode.left = null;
+      } else if (parentNode.right && parentNode.right.data === data) {
+        parentNode.right = null;
+      } else if (parentNode.data > data) {
+        removeLeaf(parentNode.left, data);
+      } else {
+        removeLeaf(parentNode.right, data);
+      }
+    };
+
+    const removeNotLeaf = (parentNode, data) => {
+      if (parentNode.data === data) {
+        if (parentNode.left) {
+          const max = findMax(parentNode.left);
+          parentNode.data = max;
+          parentNode.left.data === max
+            ? (parentNode.left = null)
+            : removeItem(parentNode.left, max);
+        } else if (parentNode.right) {
+          const min = findMin(parentNode.right);
+          parentNode.data = min;
+          parentNode.right.data === min
+            ? (parentNode.right = null)
+            : removeItem(parentNode.right, min);
+        }
+      } else if (parentNode.data > data) {
+        removeNotLeaf(parentNode.left, data);
+      } else {
+        removeNotLeaf(parentNode.right, data);
+      }
+    };
+
+    const removeItem = (parentNode, data) => {
+      const node = findNode(parentNode, data)
+      if(isLeaf(node)){
+        removeLeaf(parentNode, data);
+      } else{
+        removeNotLeaf(parentNode, data);
+      }
+    }
+
+    removeItem(this.rootNode, data);
   }
 
   min() {
-    const findMin = (node) => {
-      console.log(node);
-      return node.left ? findMin(node.left) : node.data;
-    };
     return findMin(this.rootNode);
   }
 
   max() {
-    const findMax = (node) => {
-      return node.right ? findMax(node.right) : node.data;
-    };
     return findMax(this.rootNode);
   }
 }
